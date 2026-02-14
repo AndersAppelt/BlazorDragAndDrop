61129ee1ecbf35680216a9f14935c6d65301bc79
AndersAppelt <ath@systemate.dk>
2026-02-14 14:00:21 +0100

    Initial commit

---
+++ BlazorDragAndDrop/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using BlazorDragAndDrop;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
+
+await builder.Build().RunAsync();
